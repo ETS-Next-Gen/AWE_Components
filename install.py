f9e1cbc2bb856a6943c0687b5dcab1084024ec5c
Bradley Erickson <bbwe24@gmail.com>
2023-08-10 16:33:51 -0400

    added docstring

--- install.py
+++ install.py
@@ -11,6 +11,9 @@ modified_env = {'PATH': f'{new_path}{os.pathsep}{current_path}'}
 
 
 class AWEInstall(_install):
+    '''This will download lexicons required for AWE to run, such as en_core_web_sm and en_core_web_lg.
+    Note that this operation will take some (bandwidth-dependent) time to complete.
+    '''
     def run(self):
         _install.run(self)
         subprocess.run(['python', 'awe_components/setup/data.py'], env=modified_env)
