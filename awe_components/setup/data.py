48cc62eee62953974129aa69a01fca1261c76a3b
Bradley Erickson <bbwe24@gmail.com>
2024-10-09 10:53:39 -0400

    added function to download so we can call it from code if needed

--- awe_components/setup/data.py
+++ awe_components/setup/data.py
@@ -4,9 +4,14 @@
 from spacy.cli.download import download
 import os
 
-if __name__ == '__main__':
+
+def download_models():
     print('Downloading Spacy and Coreferee Lexicons')
     download('en_core_web_sm')
     download('en_core_web_lg')
     download('en_core_web_trf')
     os.system("python3 -m coreferee install en")
+
+
+if __name__ == '__main__':
+    download_models()
